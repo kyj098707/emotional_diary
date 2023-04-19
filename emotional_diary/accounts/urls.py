906d90092ada9ea5571da6c9aeb53a9a2a17ce11
kyj098707 <kyj098707@naver.com>
2023-04-19 16:22:57 +0900

    FEAT: 뉴스피드 데이터전송

--- emotional_diary/accounts/urls.py
+++ emotional_diary/accounts/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(),name="token_obtain_pair"),
     path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('token/verify/', TokenVerifyView.as_view(), name="token_verify"),
-
     path('password/validate', views.signup_validate, name="password_validate"),
 
 ]
--- emotional_diary/diaryapp/serializers.py
+++ emotional_diary/diaryapp/serializers.py
@@ -17,16 +17,23 @@ class DiaryListSerializers(serializers.ModelSerializer):
     user = UserSerializer(required=False)
     num_like = serializers.SerializerMethodField()
     num_comment = serializers.SerializerMethodField()
+    comment = serializers.SerializerMethodField()
 
     class Meta:
         model = Diary
-        fields = ["id","title","content","user","num_like", "num_comment","created_at"]
+        fields = ["id","title","content","user","num_like", "num_comment","created_at","comment"]
 
     def get_num_comment(self, diary):
         comment_list = Comment.objects.filter(diary=diary)
         return len(comment_list)
     def get_num_like(self, diary):
         return diary.like.count()
+
+    def get_comment(self, diary):
+        comment = Comment.objects.filter(diary=diary).order_by("-created_at")
+        serializers = DiaryCommentSerializers(instance=comment, many=True)
+        return serializers.data
+
 
 class TagSerializers(serializers.ModelSerializer):
     class Meta:
@@ -39,6 +46,9 @@ class TagSerializers(serializers.ModelSerializer):
 class DiaryRetrieveSerializers(serializers.ModelSerializer):
     comment = serializers.SerializerMethodField()
     tag = TagSerializers(many=True)
+    num_like = serializers.SerializerMethodField()
+    num_comment = serializers.SerializerMethodField()
+    user = UserSerializer()
 
     class Meta:
         model = Diary
@@ -48,10 +58,17 @@ class DiaryRetrieveSerializers(serializers.ModelSerializer):
         comment = Comment.objects.filter(diary=diary).order_by("-created_at")
         serializers = DiaryCommentSerializers(instance=comment,many=True)
         return serializers.data
+    def get_num_comment(self, diary):
+        comment_list = Comment.objects.filter(diary=diary)
+        return len(comment_list)
+    def get_num_like(self, diary):
+        return diary.like.count()
+
 
 
 
 class DiaryCommentSerializers(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Comment
         fields = ["id","user","content","created_at"]
